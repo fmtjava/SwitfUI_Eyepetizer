a0a409be696475a6729c56e6ff28b1630d66b45d
fangmingtao@heytea.com <fangmingtao@heytea.com>
2025-02-05 10:29:50 +0800

    Initial Commit

---
+++ SwiftUI_Eyepetizer/SwiftUI_EyepetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EyepetizerApp.swift
+//  SwiftUI_Eyepetizer
+//
+//  Created by heytea on 2025/2/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EyepetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
